4db7c6e6528f3dc9879ccc48c78b5682d60a7e01
Chigbo-max <ezeokeke.chigbo@gmail.com>
2025-05-19 17:48:37 +0200

    fresh commit

---
+++ antiTheftTrackerApp/src/main/java/com/antiTheftTracker/antiTheftTrackerApp/AntiTheftTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.antiTheftTracker.antiTheftTrackerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiTheftTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiTheftTrackerAppApplication.class, args);
+	}
+
+}
